7202eebe27e3606503a683a503ef17fc425b394b
wafrelka <wafrelka@gmail.com>
2022-07-28 21:42:55 +0900

    Add workflow

---
+++ .github/script/release.js
@@ -0,0 +1,21 @@
+const repo = {
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+};
+const name = context.ref;
+const ref = context.ref;
+
+const {data: releases} = await github.rest.repos.listReleases(repo);
+for(const release of releases) {
+    if(release.name === name) {
+        await github.rest.repos.deleteRelease({
+            release_id: release.node_id,
+            ...repo,
+        });
+    }
+}
+
+await github.rest.repos.createRelease({
+    tag_name: context.ref,
+    ...repo,
+});

8480a421d5e19562fe0d0f49199d3ef1ecfddf80
wafrelka <wafrelka@gmail.com>
2022-07-28 21:48:12 +0900

    fix

--- .github/script/release.js
+++ .github/script/release.js
@@ -1,21 +1,25 @@
-const repo = {
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-};
-const name = context.ref;
-const ref = context.ref;
+module.exports = async ({github, context}) => {
 
-const {data: releases} = await github.rest.repos.listReleases(repo);
-for(const release of releases) {
-    if(release.name === name) {
-        await github.rest.repos.deleteRelease({
-            release_id: release.node_id,
-            ...repo,
-        });
+    const repo = {
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+    };
+    const name = context.ref;
+
+    const {data: releases} = await github.rest.repos.listReleases(repo);
+    for(const release of releases) {
+        if(release.name === name) {
+            await github.rest.repos.deleteRelease({
+                release_id: release.node_id,
+                ...repo,
+            });
+        }
     }
-}
 
-await github.rest.repos.createRelease({
-    tag_name: context.ref,
-    ...repo,
-});
+    await github.rest.repos.createRelease({
+        tag_name: context.ref,
+        name: name,
+        prerelease: true,
+        ...repo,
+    });
+};
